27d62c2c6851b12d13f9a3bbeb25e44cfbc9fcd4
Muneera Shaheryar <muneera.shaheryar@gmail.com>
2025-01-15 17:45:38 -0600

    Update main.py
    i just redid like half of it

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("Welcome to the food ordering chatbot !")
-name = input("What is your name?  ")
-age = input(f"Hello {name} , what is your age?  ")
+print("Welcome to the Instrument Info Installment!!")
+name = input('What is your name?  ')
+
+current_year = input(f'Hello {name}!!, what year is it currently?  ')
+
+birth_year = input(f'And {name}, what year were you born?    ')
+
+age = current_year - birth_year
+if age < 10:
+    print(f"oops!! {name}, you're too young to be here! please start over !!")
